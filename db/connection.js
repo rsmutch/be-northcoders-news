b9d64d48abaf4f9997ca4a8f52090354208db3f5
Richard Mutch <rsmutch@gmail.com>
2020-11-13 16:05:18 +0000

    More tweaks

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,9 @@
+const ENV = process.env.NODE_ENV || 'development';
 const knex = require('knex');
-const customConfig = require('../knexfile');
 
-const connection = knex(customConfig);
+const dbConfig =
+  ENV === 'production'
+    ? { client: 'pg', connection: process.env.DATABASE_URL }
+    : require('../knexfile');
 
-module.exports = connection;
+module.exports = knex(dbConfig);
--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,4 @@
+const { DB_URL } = process.env;
 const ENV = process.env.NODE_ENV || 'development';
 
 const baseConfig = {
@@ -11,6 +12,15 @@ const baseConfig = {
 };
 
 const customConfig = {
+  production: {
+    connection: {
+      connectionString:
+        'postgres://mwfukvhuhdvvtt:ca502e23db8583a792427f0e87d3b0cd5473f9273f2a32560c2ce9142c3aa470@ec2-52-72-221-20.compute-1.amazonaws.com:5432/d3g8j5hhhoh71v',
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    },
+  },
   development: {
     connection: {
       database: 'nc_news',
